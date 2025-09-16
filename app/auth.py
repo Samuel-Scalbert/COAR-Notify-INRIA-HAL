ea40a4d09845df2ff34aca1aef911410770e8441
Samuel-Scalbert <sam.scalbert@gmail.com>
2025-09-16 13:47:28 +0200

    refactor API key authentication to use external JSON configuration file

--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,16 @@
 from functools import wraps
 from flask import request, jsonify
+import json
 
-API_KEY = "supersecret123"
+with open("auth_admin.json") as f:
+    config = json.load(f)
+    API_KEY = config["TOKEN"]
 
 def require_api_key(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         key = request.headers.get("x-api-key")
         if key != API_KEY:
-            return jsonify({"error": "Unauthorized"}), 401
+            return jsonify({"error": "Unauthorized Token"}), 401
         return f(*args, **kwargs)
     return decorated
