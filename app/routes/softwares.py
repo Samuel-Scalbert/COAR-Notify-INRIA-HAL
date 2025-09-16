360ef3002e83faa8c25b67ed1be0fbd2680087f3
Samuel-Scalbert <sam.scalbert@gmail.com>
2025-09-16 15:25:14 +0200

    add software retrieval endpoints for individual software and mention queries

--- app/routes/softwares.py
+++ app/routes/softwares.py
@@ -11,3 +11,26 @@ def software_status():
         "total_documents": total_count,
     }
     return jsonify(status_info)
+
+@app.route('/api/softwares/<id_software>', methods=['GET'])
+def software_from_id(id_software):
+    from app.app import db
+    query = f"""
+        let soft_name = document("softwares/{id_software}")
+
+        FOR soft in softwares
+            filter soft.software_name.normalizedForm == soft_name.software_name.normalizedForm
+            return soft
+        """
+    result = db.AQLQuery(query, rawResults=True)
+    return jsonify(result[0:])
+
+@app.route('/api/softwares_mention/<id_mention>', methods=['GET'])
+def software_mention_from_id(id_mention):
+    from app.app import db
+    software_col = db["softwares"]
+    try:
+        doc = software_col.fetchDocument(id_mention)  # fetch by _key
+        return jsonify(doc.getStore())  # convert to dict
+    except Exception:
+        return jsonify({"error": "Document not found"}), 404
